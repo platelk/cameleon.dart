3bf197e59f19b11977628e653fe13c908f4fe87a
userlnx <userlnx@RENNLLXRDL3690.rennes.eu.thmulti.com>
2014-04-30 10:45:30 +0200

    Modification of example, adding 'SessionRoute.dart' example. Modification ReadMe

---
+++ example/SessionRoute.dart
@@ -0,0 +1,29 @@
+import 'packages/sdhs/Sdhs.dart';
+import "dart:io";
+import "dart:async";
+
+@Route(r"/login/(\w+)/(\w+)", others_param: "HttpSession,Sdhs")
+String login(String login, String password, HttpSession session, Sdhs s) {
+  if (login == "admin" && password == "admin") {
+    session["login"] = true;
+    session["level"] = "admin";
+    s.addRoute(my_function, session: session);
+    return "Ok";
+  }
+  return "fail";
+}
+
+@Route("disconnect", others_param: "HttpSession")
+String disconnect(HttpSession session) {
+  session.destroy();
+  return "Disconnected";
+}
+
+void main() {
+  Sdhs r = new Sdhs(4242);
+
+  r.addRoute(login);
+  r.addRoute(disconnect);
+  r.setDebug(true, level: 4);
+  r.run();
+}
--- example/simple_server.dart
+++ example/simple_server.dart
@@ -2,60 +2,22 @@ import 'packages/sdhs/Sdhs.dart';
 import "dart:io";
 import "dart:async";
 
-@Route("/test") String my_function() {
-  return "LOOOOL";
-}
 
-@Route("/data", method: "GET,POST", others_param: "HttpRequest,HttpResponse")
-String get_data(HttpRequest res, HttpResponse r) {
-  res.forEach((e) => print("-> " + new String.fromCharCodes(e)));
-  print(res);
-  r.headers.set(HttpHeaders.LOCATION, "http://127.0.0.1:4242/");
-  r.statusCode = HttpStatus.MOVED_PERMANENTLY;
-  return "ok";
-}
 
-@Route(r"/redirect")
+@Route(r"/")
 Future<String> other_func([String request = "", var a = null]) {
   return new Future(() => "welcome");
 }
 
-@Route(r"/login/(\w+)/(\w+)", others_param: "HttpSession,Sdhs")
-String login(String login, String password, HttpSession session, Sdhs s) {
-  if (login == "admin" && password == "admin") {
-    session["login"] = true;
-    session["level"] = "admin";
-    s.addRoute(my_function, session: session);
-    return "Ok";
-  }
-  return "fail";
-}
-
-@Route("disconnect", others_param: "HttpSession")
-String disconnect(HttpSession session) {
-  session.destroy();
-  return "Disconnected";
-}
-
-@Route("")
-
-class R {
-    @Route(r'class')
-    String login() {
-      return "Hi ! i'm glad to see you";
-    }
-}
 
 void main() {
   Sdhs r = new Sdhs(4242);
 
-  r.addRouteFile("/index", "assets/index.html", method: "GET");
-  r.addRoute(get_data);
-  r.addRoute(() => "Salut", session: null, routePath: "/other", base_url: "", method : "GET");
-  r.addRoute(#other_func);
-  r.addRoute(new R());
-  r.addRoute(login);
-  r.addRoute(disconnect);
-  r.setDebug(true, level: 4);
-  r.run();
+  r
+  ..addRouteFile("/index", "assets/index.html", method: "GET")
+  ..addRoute(() => "Salut", routePath: "/other")
+  ..addRoute(#other_func)
+  // Or just : r.addRoute(other_func)
+  ..setDebug(true, level: 4)
+  ..run();
 }
